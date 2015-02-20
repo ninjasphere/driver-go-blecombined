469a5d322171f1bccdc97cd49a8a0c69bcf7be1f
Mark Wolfe <mark@wolfe.id.au>
2015-02-20 14:01:07 +1100

    Removed old logging setup.

--- release.go
+++ release.go
@@ -8,11 +8,7 @@ import (
 	"github.com/ninjasphere/go-ninja/logger"
 )
 
-func debug(string, ...interface{}) {}
-
 func init() {
-	logger.GetLogger("").SetLogLevel(loggo.INFO)
-
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey:       "7b66d4013bdcd0541287fd9b00376253",
 		ReleaseStage: "production",
